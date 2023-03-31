ba34e115ff38789e365d94ef45e2beedebf0d16d
adrianaj20 <112966142+adrianaj20@users.noreply.github.com>
2023-03-31 07:35:30 -0500

    Update db.php

--- public/db.php
+++ public/db.php
@@ -1,2 +1,23 @@
 <?php
+
+// Configuración de la conexión
+$servername = "dpg-cggrd7keoogqfc463qlg-a";
+$username = "admin";
+$password = "KNxRqeert9SXREdCe6eeYndEUv4vvRV1";
+$dbname = "bd2023";
+
+// Intentar la conexión
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+
+// Comprobar si hay errores en la conexión
+if (!$conn) {
+    die("La conexión ha fallado: " . mysqli_connect_error());
+}
+
+echo "¡Conexión exitosa!";
+
+// Cerrar la conexión
+mysqli_close($conn);
+
 ?>
+
